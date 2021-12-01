094691f327879f49470ecc77e1dd771c8ce6234c
Juan <camiloflstudio@gmail.com>
2021-11-30 19:51:42 -0500

    Branch Dev

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Gestion de proyectos
         </p>
         <a
           className="App-link"
